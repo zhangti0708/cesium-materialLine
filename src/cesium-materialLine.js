556ce7963e6ed032469ca5b2f84f87fed2563707
ZhangTi <44698429+zhangti0708@users.noreply.github.com>
2021-08-02 10:28:25 +0800

    Update cesium-materialLine.js

--- src/cesium-materialLine.js
+++ src/cesium-materialLine.js
@@ -1,1 +1,102 @@
-;if(typeof Cesium!=='undefined')(function(_0x28e6fb){var _0x3a4a3c={'iKSVw':'1|4|0|5|3|2','loCcz':function(_0x8baaf6,_0x53c135){return _0x8baaf6(_0x53c135);},'ZVLvT':function(_0x5a6faa,_0x35dbb2){return _0x5a6faa/_0x35dbb2;},'OOtAe':function(_0x5ac73d,_0x1d0c26){return _0x5ac73d*_0x1d0c26;},'YUfBq':function(_0x150977,_0x34f301){return _0x150977-_0x34f301;},'AAzBp':function(_0x4456b6,_0x489fd6){return _0x4456b6===_0x489fd6;},'zWdDc':'0|1|5|4|6|2|3','phCaN':function(_0x22f7fe,_0x22460b,_0x584f20){return _0x22f7fe(_0x22460b,_0x584f20);},'xUWjD':function(_0x2d653f,_0x3fc619){return _0x2d653f===_0x3fc619;},'iTcol':function(_0x4dea27,_0x5c701b){return _0x4dea27 instanceof _0x5c701b;},'YLCPu':function(_0xbac2e7,_0x417f56){return _0xbac2e7+_0x417f56;},'kDWEL':'wallType','jikaj':function(_0x37f580,_0xb90f4c){return _0x37f580*_0xb90f4c;},'uvOSD':'color','foFmO':'czm_material\x20czm_getMaterial(czm_materialInput\x20materialInput)\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20czm_material\x20material\x20=\x20czm_getDefaultMaterial(materialInput);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20vec2\x20st\x20=\x20materialInput.st;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20if(texture2D(image,\x20vec2(0.0,\x200.0)).a\x20==\x201.0){\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20discard;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}else{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20material.alpha\x20=\x20texture2D(image,\x20vec2(1.0\x20-\x20fract(time\x20-\x20st.s),\x20st.t)).a\x20*\x20color.a;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20material.diffuse\x20=\x20max(color.rgb\x20*\x20material.alpha\x20*\x203.0,\x20color.rgb);\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20return\x20material;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20','aQpuV':'background:\x20#606060;\x20color:\x20#fff;\x20border-radius:\x203px\x200\x200\x203px;','XOzEg':'background:\x20#1475B2;\x20color:\x20#fff;\x20border-radius:\x200\x203px\x203px\x200;'};function _0x265ba3(_0x4f3655){var _0x5f1c70={'KioEk':_0x3a4a3c['zWdDc'],'mPDit':function(_0x1b1c92,_0x330c7a,_0x2b6846){return _0x3a4a3c['phCaN'](_0x1b1c92,_0x330c7a,_0x2b6846);},'zvyOY':function(_0x337a2c,_0x58ba2d){return _0x3a4a3c['xUWjD'](_0x337a2c,_0x58ba2d);},'UBCpg':function(_0x51b947,_0x1cee24){return _0x3a4a3c['iTcol'](_0x51b947,_0x1cee24);}};var _0x578460=_0x28e6fb['Color'],_0x18e133=_0x28e6fb['defaultValue'],_0x4e36fd=_0x28e6fb['defined'],_0x285486=Object['defineProperties'],_0xd32598=_0x28e6fb['Event'],_0x35590=_0x28e6fb['createPropertyDescriptor'],_0x1479bd=_0x28e6fb['Property'],_0x5236ab=_0x28e6fb['Material'],_0x26ead8=_0x578460['WHITE'],_0xc22a2e=_0x4f3655['MaterialType']||_0x3a4a3c['YLCPu'](_0x3a4a3c['kDWEL'],_0x3a4a3c['loCcz'](parseInt,_0x3a4a3c['jikaj'](Math['random'](),0x3e8)));function _0x467c57(_0x22dfb5){var _0x101c3d=_0x5f1c70['KioEk']['split']('|'),_0x53d876=0x0;while(!![]){switch(_0x101c3d[_0x53d876++]){case'0':_0x22dfb5=_0x5f1c70['mPDit'](_0x18e133,_0x22dfb5,_0x18e133['EMPTY_OBJECT']);continue;case'1':this['_definitionChanged']=new _0xd32598();continue;case'2':this['duration']=_0x22dfb5['duration']||0x3e8;continue;case'3':this['_time']=undefined;continue;case'4':this['_colorSubscription']=undefined;continue;case'5':this['_color']=undefined;continue;case'6':this['color']=_0x22dfb5['color']||_0x28e6fb['Color']['BLUE'];continue;}break;}}_0x3a4a3c['phCaN'](_0x285486,_0x467c57['prototype'],{'isvarant':{'get':function(){return![];}},'definitionChanged':{'get':function(){return this['_definitionChanged'];}},'color':_0x3a4a3c['loCcz'](_0x35590,_0x3a4a3c['uvOSD'])});_0x467c57['prototype']['getType']=function(_0x17bed1){return _0xc22a2e;};_0x467c57['prototype']['getValue']=function(_0x52a446,_0x2c759a){var _0x57bbe9=_0x3a4a3c['iKSVw']['split']('|'),_0x2a5c25=0x0;while(!![]){switch(_0x57bbe9[_0x2a5c25++]){case'0':_0x2c759a['image']=_0x4f3655['image'];continue;case'1':if(!_0x3a4a3c['loCcz'](_0x4e36fd,_0x2c759a)){_0x2c759a={};}continue;case'2':return _0x2c759a;case'3':_0x2c759a['time']=_0x3a4a3c['ZVLvT'](_0x3a4a3c['OOtAe'](_0x3a4a3c['YUfBq'](_0x52a446['secondsOfDay'],this['_time']),0x3e8),this['duration']);continue;case'4':_0x2c759a['color']=_0x1479bd['getValueOrClonedDefault'](this['_color'],_0x52a446,_0x26ead8,_0x2c759a['color']);continue;case'5':if(_0x3a4a3c['AAzBp'](this['_time'],undefined)){this['_time']=_0x52a446['secondsOfDay'];}continue;}break;}};_0x467c57['prototype']['equals']=function(_0x47fd34){return _0x5f1c70['zvyOY'](this,_0x47fd34)||_0x5f1c70['UBCpg'](_0x47fd34,_0x467c57)&&_0x1479bd['equals'](this['_color'],_0x47fd34['_color']);};_0x5236ab['_materialCache']['addMaterial'](_0xc22a2e,{'fabric':{'type':_0xc22a2e,'uniforms':{'color':_0x4f3655['color']||new _0x28e6fb['Color'](0x1,0x0,0x0,0.5),'image':_0x4f3655['image'],'time':_0x4f3655['color']['time']||0x0},'source':_0x3a4a3c['foFmO']},'translucent':function(_0x5d5e53){return!![];}});return new _0x467c57(_0x4f3655);}_0x28e6fb['CustomMaterialLine']=_0x265ba3;let _0x552062=_0x3a4a3c['aQpuV'];let _0x5de97c=_0x3a4a3c['XOzEg'];console['log']('%c\x20web3d/webgis相关工作推荐邮箱\x20:\x20%c\x20zhangticcc@163.com\x20',_0x552062,_0x5de97c);console['log']('更多插件https://github.com/zhangti0708\x20%c\x20=,=给个广告位',_0x5de97c);}(Cesium));
+; if (typeof Cesium !== 'undefined') (function (Cesium) {
+    /**
+     * 自定义材质线Property 适用于entity和primitive材质
+     * @param {*} options 
+     * @returns 
+     */
+    function CustomMaterialLine(options) {
+
+        var Color = Cesium.Color,
+            defaultValue = Cesium.defaultValue,
+            defined = Cesium.defined,
+            defineProperties = Object.defineProperties,
+            Event = Cesium.Event,
+            createPropertyDescriptor = Cesium.createPropertyDescriptor,
+            Property = Cesium.Property,
+            Material = Cesium.Material,
+            defaultColor = Color.WHITE,
+            MaterialType = options.MaterialType || 'wallType' + parseInt(Math.random() * 1000);
+        // 创建自定义动态材质对象
+        function PolylineCustomMaterialProperty(options) {
+
+            options = defaultValue(options, defaultValue.EMPTY_OBJECT);
+            // 定义内置属性
+            this._definitionChanged = new Event();
+            this._color = undefined;
+            this._colorSubscription = undefined;
+            this.color = options.color || Cesium.Color.BLUE;
+            this.duration = options.duration || 1000;
+            this._time = undefined;
+        }
+        // 定义材质对象默认属性
+        defineProperties(PolylineCustomMaterialProperty.prototype, {
+            isvarant: {
+                get: function () {
+                    return false;
+                }
+            },
+            definitionChanged: {
+                get: function () {
+                    return this._definitionChanged;
+                }
+            },
+            color: createPropertyDescriptor('color')
+        });
+        // 材质对象需要有type函数 value函数 equals函数
+        PolylineCustomMaterialProperty.prototype.getType = function (time) {
+            return MaterialType;
+        };
+        PolylineCustomMaterialProperty.prototype.getValue = function (time, result) {
+            if (!defined(result)) {
+                result = {};
+            }
+            result.color = Property.getValueOrClonedDefault(this._color, time, defaultColor, result.color);
+            result.image = options.image;
+            if (this._time === undefined) {
+                this._time = time.secondsOfDay;
+            }
+            result.time = (time.secondsOfDay - this._time) * 1000 / this.duration;
+            return result;
+        };
+        PolylineCustomMaterialProperty.prototype.equals = function (other) {
+            return this === other || //
+                (other instanceof PolylineCustomMaterialProperty &&
+                    Property.equals(this._color, other._color));
+        };
+        // 将定义的材质对象添加到cesium的材质队列中
+        Material._materialCache.addMaterial(MaterialType, {
+            fabric: {
+                type: MaterialType,
+                uniforms: {
+                    color: options.color || new Cesium.Color(1.0, 0.0, 0.0, 0.5),
+                    image: options.image,
+                    time: options.color.time || 0
+                },
+                // 动态材质shader
+                source: "czm_material czm_getMaterial(czm_materialInput materialInput)\n\
+                {\n\
+                    czm_material material = czm_getDefaultMaterial(materialInput);\n\
+                    vec2 st = materialInput.st;\n\
+                    \n\
+                    if(texture2D(image, vec2(0.0, 0.0)).a == 1.0){\n\
+                        discard;\n\
+                    }else{\n\
+                        material.alpha = texture2D(image, vec2(1.0 - fract(time - st.s), st.t)).a * color.a;\n\
+                    }\n\
+                    \n\
+                    material.diffuse = max(color.rgb * material.alpha * 3.0, color.rgb);\n\
+                    \n\
+                    return material;\n\
+                }\n\
+                ",
+            },
+            // 透明
+            translucent: function (material) {
+                return true;
+            }
+        })
+        return new PolylineCustomMaterialProperty(options);
+    }
+
+    Cesium.CustomMaterialLine = CustomMaterialLine;
+})(Cesium);
